05d5bd1787224c146d16bf0259efac47b9a30054
neishakb <neishakbrown@gmail.com>
2020-01-29 09:27:50 -0800

    created my first Flask app

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ app = Flask(__name__)
 # Routing for your application.
 # Put your routes below this comment
 '''
+@app.route("/") #decorator 
+def home():
+    return "My home page"
+
 
 
 @app.errorhandler(404)
@@ -22,4 +26,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host ="0.0.0.0", port=8080)
